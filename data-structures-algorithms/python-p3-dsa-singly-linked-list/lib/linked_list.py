e85668ceee534741385a253d3fb53231a862a377
betalantz <betalantz@gmail.com>
2024-10-03 09:23:20 -0700

    adds methods to linked list implementation

--- data-structures-algorithms/python-p3-dsa-singly-linked-list/lib/linked_list.py
+++ data-structures-algorithms/python-p3-dsa-singly-linked-list/lib/linked_list.py
@@ -1,6 +1,7 @@
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
+        self.tail = head
 
     def append(self, node):
         if not self.head:
@@ -25,10 +26,41 @@ class LinkedList:
         print(result)
 
     def find(self, target):
-        pass
+        if not self.head:
+            print("Empty list")
+            return
+        curr = self.head
+        while curr.next:
+            if curr.value == target:
+                return curr
+            curr = curr.next
+        return "Not found"
 
-    def insert_after(self, target_node, new_node):
-        pass
+    # def insert_after(self, target_node, new_node):
+    #     if not target_node or not new_node:
+    #         print("args must be nodes")
+    #     new_node.next = target_node.next
+    #     target_node.next = new_node
 
-    def insert_at_index(self, index, node):
+    # def insert_at_index(self, index, node):
+    #     curr = self.head
+    #     for _ in range(index):
+    #         if not curr.next:
+    #             print("index out of range")
+    #             return
+    #         curr = curr.next
+    #     node.next = curr.next
+    #     curr.next = node
+
+    # def remove_at_index(self, index):
+    #     pass
+
+    # def prepend(self, node):
+    #     pass
+
+    # def remove_head(self):
+    #     # handle edge cases no head or only one node
+    #     self.head = self.head.next
+
+    def traverse(self):
         pass
