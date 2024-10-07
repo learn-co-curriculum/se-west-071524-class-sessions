43001ee8363c5490fa060e74065ea6889d88a7c3
betalantz <betalantz@gmail.com>
2024-10-07 10:53:18 -0700

    implements addTen on budgetSlice

--- 05-react-redux/src/components/Budget.jsx
+++ 05-react-redux/src/components/Budget.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-// import { addTen, subtractAmount } from '../features/budget/budgetSlice'
+import { addTen, subtractAmount } from '../features/budget/budgetSlice'
 
 export default function Budget() {
+
+  const budget = useSelector(state => state.budget.value)
+  const dispatch = useDispatch()
   
 
-  const handleAddTen = () => console.log("addTen clicked")
+  const handleAddTen = () => dispatch(addTen())
 
   const handleSubtract = () => console.log("subtract clicked")
 
   return (
     <div>
         <h2>Redux Shelter Budget:</h2>
-        {/* <h3>${budget}</h3> */}
+        <h3>${budget}</h3>
         <div>
             <button className="ui button" onClick={handleAddTen}>Add $10</button>
             <button className="ui button" onClick={handleSubtract}>Subtract $5</button>
