1e344ad54942f8e8b941e093fdc89c74c7a2ca86
betalantz <betalantz@gmail.com>
2024-10-07 10:42:25 -0700

    initialize store w budgetSlice

---
+++ 05-react-redux/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import budgetReducer from '../features/budget/budgetSlice'
+
+export const store = configureStore({
+
+    reducer: {
+        budget: budgetReducer
+    },
+
+})
--- 05-react-redux/src/main.jsx
+++ 05-react-redux/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
-
+import { store } from './app/store'
+import { Provider } from  'react-redux'
+ 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
